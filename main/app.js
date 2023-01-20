3b66b91591b052e30b52e915ccc630b0d42d0a2f
farhanacsebd <farhana.cse.bd@gmail.com>
2023-01-21 00:36:35 +0600

    total file formet

--- main/app.js
+++ main/app.js
@@ -1,17 +1,16 @@
-const imageContainer = document.getElementById('image-container');
-const btn = document.getElementById('btn');
+const imageContainer = document.getElementById("image-container");
+const btn = document.getElementById("btn");
 
-btn.addEventListener("click",() => {
-    console.log('hello');
-    imageNum = 10;
-    addNewImage()
-})
+btn.addEventListener("click", () => {
+  console.log("hello");
+  imageNum = 10;
+  addNewImage();
+});
 
-const addNewImage = () =>{
-    for (let index = 0; index < imageNum; index++) {
-        const newImages = document.createElement('img');
-    newImages.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
-    imageContainer.appendChild(newImages)
-        
-    }
-}
+const addNewImage = () => {
+  for (let index = 0; index < imageNum; index++) {
+    const newImages = document.createElement("img");
+    newImages.src = `https://picsum.photos/300?random=${Math.floor(Math.random() * 2000)}`;
+    imageContainer.appendChild(newImages);
+  }
+};
